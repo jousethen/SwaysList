6975d0823fc803611141ae656afb831858169bf3
jousethen <jthen93@gmail.com>
2021-07-08 23:23:41 -0400

    Created main layout. With Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :items
   resources :storefronts, only:[:show, :edit]
   resources :users
+  
+  root 'application#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
