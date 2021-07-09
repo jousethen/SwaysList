e78878b57e7b6733fe0a55324daca3f5873c5333
jousethen <jthen93@gmail.com>
2021-07-09 01:09:53 -0400

    Completed login page with flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,21 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def logout
+  end
+
+  def create
+    params.inspect
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to storefronts_path
+    else
+      redirect_to '/login', alert: "Unable to Authenticate"
+    end
+  end
+
+  def destroy
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_one :storefront
   has_many :transactions
   has_secure_password
-  accepts_nested_attributes_for :storefronts
+  # accepts_nested_attributes_for :storefronts
   validates :email, presence: true
 end
