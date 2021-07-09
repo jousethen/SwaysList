91768c5f7533b47f3f8f96ad756a2e0fbbbe12a3
jousethen <jthen93@gmail.com>
2021-07-09 16:57:09 -0400

    Tested users/new. Balance not working exact but the user was able to be created and redirected to homepage on create

--- app/controllers/storefronts_controller.rb
+++ app/controllers/storefronts_controller.rb
@@ -1,2 +1,4 @@
 class StorefrontsController < ApplicationController
+  def index 
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,27 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    user = User.new(user_params)
+    
+    if user.authenticate(params[:user][:password])
+      user.save
+      session[:user_id] = user.id
+
+      if user.vendor
+        session[:admin] = user.id
+      end
+      
+      redirect_to '/'
+    else
+      redirect_to new_user_path
+    end
+  end
+
+  private 
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :balance, :address, :password, :vendor)
+  end
+
+
 end
