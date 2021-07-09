f867ca357cd9b8692ae35cb4175371b98da257a2
jousethen <jthen93@gmail.com>
2021-07-08 22:28:23 -0400

    Modified routes.rb and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_one :storefront
   has_many :transactions
+  has_secure_password
+  accepts_nested_attributes_for :storefronts
+  validates :email, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  resources :categories
   resources :transactions
   resources :items
-  resources :storefronts
+  resources :storefronts, only:[:show, :edit]
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
