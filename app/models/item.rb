1d48fa8b8f89b6ee2a466dcdda26f7247b5bf350
jousethen <jthen93@gmail.com>
2021-07-13 12:44:46 -0400

    Removed new item button from inside the edit form to prevent authentication error. Ran into issue with form :

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,8 @@
 class Item < ApplicationRecord
   has_and_belongs_to_many :categories
   belongs_to :storefront
-  has_and_belongs_to_many :transactions
-  has_many :users, through: :transactions
+  # has_and_belongs_to_many :transactions
+  #has_many :users, through: :transactions
   accepts_nested_attributes_for :categories
 
   def categories_attributes=(category_attributes)
