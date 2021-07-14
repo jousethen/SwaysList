6912ffb24c384d5a24154b2c7dd8def2ae9800ca
jousethen <jthen93@gmail.com>
2021-07-14 12:30:30 -0400

    Bug Fix with authenticating. Added Items to storefront page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email].downcase)
     
-    if user.authenticate(params[:password])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to "/"
     else
