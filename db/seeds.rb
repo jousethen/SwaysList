dbfda1d1b8edf9f150eae87864514cc92b4c071b
jousethen <jthen93@gmail.com>
2021-07-14 16:40:38 -0400

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = Category.create([{ name: 'Clothing' }, { name: 'Hardware' }, { name: 'Entertainment' }, { name: 'Essentials' },{ name: 'Jewelry' }, { name: 'Technology' } ])
+
+User.create(email: 'SolBadGuy@gmail.com', first_name: 'Sol', last_name: 'BadGuy', password: '1234', balance: 100, address: 'The Backyard', vendor: false)
+User.create(email: 'KyKiske@gmail.com', first_name: 'Ky', last_name: 'Kiske', password: '1234', balance: 100, address: 'Ilyria', vendor: true)
+User.create(email: 'LeoWhitefang@gmail.com', first_name: 'Leo', last_name: 'Whitefang', password: '1234', balance: 100, address: 'Ilyria', vendor: false)
+User.create(email: 'JamKuradoberi@gmail.com', first_name: 'Jam', last_name: 'Kuradoberi', password: '1234', balance: 100, address: 'China', vendor: true)
+User.create(email: 'MilliaRage@gmail.com', first_name: 'Millia', last_name: 'Rage', password: '1234', balance: 100, address: 'The Backyard', vendor: true)
+
+
+Storefront.create(name: 'V-Necks Galore', description: 'Onestop shop for all your V-neck needs', owner_id: 2)
+Storefront.create(name: 'Puffball Restaurant', description: 'Dumplings, Porkbuns, and IAD JH', owner_id: 4)
+Storefront.create(name: 'Rage Hair Supplies', description: 'For strong, top tier hair. No one will know what hit them. ', owner_id: 5)
