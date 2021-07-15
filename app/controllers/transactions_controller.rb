dcbac4391da7eae254dfc7dce37b0eafa7c20e8d
jousethen <jthen93@gmail.com>
2021-07-15 12:21:37 -0400

    Alerts set up for adding to cart. Badge also set up for cart in navbar

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,10 +1,17 @@
 class TransactionsController < ApplicationController
   def add_item
+    item = Item.find(params[:id])
+    
     if session[:cart]
       session[:cart] << params[:id]
     else
       session[:cart] = []
       session[:cart] << params[:id]
     end
+
+    flash.alert = "#{item.name} added to cart"
+    redirect_to storefront_item_path(item.storefront.id, item.id)
   end
+  
+  
 end
