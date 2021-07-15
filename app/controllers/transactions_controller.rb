3692eddd3889339c0b20c0abe5a2b30381e31091
jousethen <jthen93@gmail.com>
2021-07-15 11:37:56 -0400

    Functional 'cart' in sessions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,4 +1,10 @@
 class TransactionsController < ApplicationController
   def add_item
+    if session[:cart]
+      session[:cart] << params[:id]
+    else
+      session[:cart] = []
+      session[:cart] << params[:id]
+    end
   end
 end
