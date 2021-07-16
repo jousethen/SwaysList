bf63ecc119a86f9449d2b36a034af4e3f4996199
jousethen <jthen93@gmail.com>
2021-07-15 20:42:11 -0400

    Cart Page Complete

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -1,2 +1,14 @@
 module TransactionsHelper
+  def cart_display
+    uniq_cart = session[:cart].uniq
+
+    display = {}
+    
+    uniq_cart.each do |i|
+      item = Item.find(i)
+      
+      display[item] = session[:cart].count(i)
+    end
+    display
+  end
 end
