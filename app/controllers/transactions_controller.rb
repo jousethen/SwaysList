c2e56112dfc28fadf7989a8735069d479793349a
jousethen <jthen93@gmail.com>
2021-07-17 00:05:48 -0400

    Created transactions/new

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -14,14 +14,17 @@ class TransactionsController < ApplicationController
   end
 
   def remove_item
+    item = Item.find(params[:id])
     session[:cart].delete_at(session[:cart].index(params[:id]) || session[:cart].length)
 
-    # flash.alert = "#{item.name} removed to cart"
+    flash.alert = "#{item.name} removed to cart"
     redirect_back(fallback_location: root_path) 
   end
 
   def cart
   end
   
+  def new
+  end
   
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,5 @@
 module UsersHelper
+  def final_balance(amount)
+    current_user.balance - amount
+  end
 end
