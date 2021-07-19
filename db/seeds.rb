c86323f6b58877ddcb7b5aaa04d3ea220ef5b4d1
jousethen <jthen93@gmail.com>
2021-07-19 16:45:56 -0400

    Bug fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,20 @@ User.create(email: 'KyKiske@gmail.com', first_name: 'Ky', last_name: 'Kiske', pa
 User.create(email: 'LeoWhitefang@gmail.com', first_name: 'Leo', last_name: 'Whitefang', password: '1234', balance: 100, address: 'Ilyria', vendor: false)
 User.create(email: 'JamKuradoberi@gmail.com', first_name: 'Jam', last_name: 'Kuradoberi', password: '1234', balance: 100, address: 'China', vendor: true)
 User.create(email: 'MilliaRage@gmail.com', first_name: 'Millia', last_name: 'Rage', password: '1234', balance: 100, address: 'The Backyard', vendor: true)
-
+User.create(email: 'jthen93@gmail.com', first_name: 'Jouse', last_name: 'Then', password: '1234', balance: 100, address: 'Brooklyn, NY', vendor: true)
 
 Storefront.create(name: 'V-Necks Galore', description: 'Onestop shop for all your V-neck needs', owner_id: 2)
 Storefront.create(name: 'Puffball Restaurant', description: 'Dumplings, Porkbuns, and IAD JH', owner_id: 4)
 Storefront.create(name: 'Rage Hair Supplies', description: 'For strong, top tier hair. No one will know what hit them. ', owner_id: 5)
+
+Item.create(name: 'Blue V-Neck', description: 'Nice tight V-neck', price: 40, storefront_id: 1)
+Item.create(name: 'Yellow V-Neck', description: 'Nice tight V-neck', price: 40, storefront_id: 1)
+Item.create(name: 'Yellow V-Neck', description: 'Nice tight V-neck', price: 40, storefront_id: 1)
+
+Item.create(name: 'Pork Dumpling', description: 'Delicious Pork Dumpling', price: 10, storefront_id: 2)
+Item.create(name: 'Chicken Dumpling', description: 'Delicious Chicken Dumpling', price: 10, storefront_id: 2)
+Item.create(name: 'Vegan Dumpling', description: 'Delicious Vegan Dumpling', price: 10, storefront_id: 2)
+Item.create(name: 'Pork Bun', description: 'Delicious Pork Bun', price: 10, storefront_id: 2)
+Item.create(name: 'Daily Special', description: 'ITS A SURPRISE', price: 30, storefront_id: 2)
+
+Item.create(name: 'Hairspray', description: 'For vicious hair', price: 50, storefront_id: 3)
